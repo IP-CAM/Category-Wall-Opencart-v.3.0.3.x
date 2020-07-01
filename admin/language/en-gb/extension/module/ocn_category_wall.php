e408878e6f676910f1c20fe26f86f027bb1e1840
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2020-07-01 18:32:30 +0300

    Файлы модуля для админки

--- admin/language/en-gb/extension/module/ocn_category_wall.php
+++ admin/language/en-gb/extension/module/ocn_category_wall.php
@@ -1,14 +1,35 @@
 <?php
 // Heading
-$_['heading_title']    = '[OCN] Category Wall';
+$_['heading_title']                  = '[OCN] Category Wall';
+
+// Button
+$_['button_apply']                   = 'Apply';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified category wall module!';
-$_['text_edit']        = 'Edit Category Wall Module';
+$_['text_extension']                 = 'Extensions';
+$_['text_success']                   = 'Success: You have modified category wall module!';
+$_['text_edit']                      = 'Edit Category Wall Module';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']                   = 'Status';
+$_['entry_title']                    = 'Title';
+$_['entry_image']                    = 'Category Image';
+$_['entry_image_width']              = 'Image width';
+$_['entry_image_height']             = 'Image height';
+$_['entry_child']                    = 'Subcategories';
+$_['entry_child_limit']              = 'Subcategory Limit';
+$_['entry_description']              = 'Subcategory Description';
+$_['entry_description_length']       = 'Description Length';
+
+// Placeholder
+$_['placeholder_child_limit']        = '0 to cancel the limit';
+$_['placeholder_description_length'] = '0 to cancel the cut';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify category wall module!';
+$_['error_permission']               = 'Warning: You do not have permission to modify category wall module!';
+$_['error_warning']                  = 'Warning: Please check the form carefully for errors!';
+$_['error_title']                    = 'Title must be between 1 and 255 characters!';
+$_['error_width']                    = 'Width required!';
+$_['error_height']                   = 'Height required!';
+$_['error_limit']                    = 'Limit required!';
+$_['error_length']                   = 'Length required!';
