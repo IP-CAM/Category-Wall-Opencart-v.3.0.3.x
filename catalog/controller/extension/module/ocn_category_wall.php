7447247f3756c3a118f3f3daeb20baf678508330
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2020-07-01 18:32:49 +0300

    Файлы модуля для витрины

--- catalog/controller/extension/module/ocn_category_wall.php
+++ catalog/controller/extension/module/ocn_category_wall.php
@@ -1,59 +1,62 @@
 <?php
 class ControllerExtensionModuleOCNCategoryWall extends Controller {
 	public function index() {
-		$this->load->language('extension/module/ocn_category_wall');
-
-		if (isset($this->request->get['path'])) {
-			$parts = explode('_', (string)$this->request->get['path']);
-		} else {
-			$parts = array();
-		}
-
-		if (isset($parts[0])) {
-			$data['category_id'] = $parts[0];
-		} else {
-			$data['category_id'] = 0;
-		}
-
-		if (isset($parts[1])) {
-			$data['child_id'] = $parts[1];
-		} else {
-			$data['child_id'] = 0;
-		}
+		$this->document->addStyle('catalog/view/theme/default/stylesheet/ocn/category-wall.css');
 
 		$this->load->model('catalog/category');
-
+		$this->load->model('extension/module/ocn_category_wall');
 		$this->load->model('catalog/product');
+		$this->load->model('tool/image');
 
-		$data['categories'] = array();
+		$data['title'] = $this->config->get('module_ocn_category_wall_title');
+		
+		$category_id = 0;
+		$limit = $this->config->get('module_ocn_category_wall_child_limit');
+		$length = $this->config->get('module_ocn_category_wall_description_length');
 
-		$categories = $this->model_catalog_category->getCategories(0);
+		$categories = $this->model_extension_module_ocn_category_wall->getCategories($category_id);
 
 		foreach ($categories as $category) {
+			// Level 2
 			$children_data = array();
 
-			if ($category['category_id'] == $data['category_id']) {
-				$children = $this->model_catalog_category->getCategories($category['category_id']);
+			if ($this->config->get('module_ocn_category_wall_child_status')) {
+				$children = $this->model_extension_module_ocn_category_wall->getCategories($category['category_id'], $limit);
 
-				foreach($children as $child) {
-					$filter_data = array('filter_category_id' => $child['category_id'], 'filter_sub_category' => true);
-
+				foreach ($children as $child) {
 					$children_data[] = array(
 						'category_id' => $child['category_id'],
-						'name' => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+						'name' => $child['name'],
 						'href' => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
 					);
 				}
 			}
-
-			$filter_data = array(
-				'filter_category_id'  => $category['category_id'],
-				'filter_sub_category' => true
-			);
-
+			
+			// Level 1
+			$image = '';
+			$image_status = $this->config->get('module_ocn_category_wall_image_status');
+			if ($image_status) {
+				if ($category['image']) {
+					$image = $this->model_tool_image->resize($category['image'], $this->config->get('module_ocn_category_wall_image_width'), $this->config->get('module_ocn_category_wall_image_height'));
+				} else {
+					$image = $this->model_tool_image->resize('placeholder.png', $this->config->get('module_ocn_category_wall_image_width'), $this->config->get('module_ocn_category_wall_image_height'));
+				}
+			}
+			
+			$description = '';
+			if ($this->config->get('module_ocn_category_wall_description_status') && $category['description'] && $category['description'] != '') {
+				$description = trim(strip_tags(html_entity_decode($category['description'], ENT_QUOTES, 'UTF-8')));
+				if ($length) {
+					$description = utf8_substr($description, 0, $length) . '...';
+				}
+			}
+			
 			$data['categories'][] = array(
-				'category_id' => $category['category_id'],
-				'name'        => $category['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+				'category_id' => $category_id,
+				'name'        => $category['name'],
+				'image'       => $image,
+				'image_status'=> $image_status,
+				'description' => $description,
 				'children'    => $children_data,
 				'href'        => $this->url->link('product/category', 'path=' . $category['category_id'])
 			);
